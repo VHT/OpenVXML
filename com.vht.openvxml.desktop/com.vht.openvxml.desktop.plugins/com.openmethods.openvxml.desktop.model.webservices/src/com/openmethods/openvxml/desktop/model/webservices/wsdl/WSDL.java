b394a6d5449c485006fbc5847b3e060f9ca383d5
brohini <rohini.bhumanapalle@mobigesture.com>
2022-10-20 21:51:49 +0530

    Improper Restriction of Stored XXE Ref

--- com.vht.openvxml.desktop/com.vht.openvxml.desktop.plugins/com.openmethods.openvxml.desktop.model.webservices/src/com/openmethods/openvxml/desktop/model/webservices/wsdl/WSDL.java
+++ com.vht.openvxml.desktop/com.vht.openvxml.desktop.plugins/com.openmethods.openvxml.desktop.model.webservices/src/com/openmethods/openvxml/desktop/model/webservices/wsdl/WSDL.java
@@ -709,6 +709,12 @@ public class WSDL {
 			DocumentBuilderFactory factory = DocumentBuilderFactory
 					.newInstance();
 			factory.setNamespaceAware(true);
+			factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			factory.setXIncludeAware(false);
+			factory.setExpandEntityReferences(false);
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document document = builder.parse(file);
 			org.w3c.dom.Element rootElement = document.getDocumentElement();
